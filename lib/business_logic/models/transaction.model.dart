4c8a77918804c920cf0a76aa099a13bb141b14e9
tluanga <tluanga@live.com>
2021-01-18 19:24:41 +0530

    a

--- lib/business_logic/models/transaction.model.dart
+++ lib/business_logic/models/transaction.model.dart
@@ -1,10 +1,19 @@
 class Transaction {
   int id;
   double amount;
+  String particular;
   DateTime date;
   int mode; //0-leina, 1-hralna
   int transactionTypeId;
   int baOrPektlak; // 0 for ba -1 for pektlak
-  int cashOrBank; //0 for cash - 1 for bank
-  Transaction({this.id, this.amount, this.transactionTypeId, this.baOrPektlak});
+  int cashOrBank; //0 for cash - 1 for bank-2 for credit
+  Transaction(
+      {this.id,
+      this.amount,
+      this.particular,
+      this.date,
+      this.mode,
+      this.transactionTypeId,
+      this.baOrPektlak,
+      this.cashOrBank});
 }
--- lib/business_logic/view_models/transaction.viewmodel.dart
+++ lib/business_logic/view_models/transaction.viewmodel.dart
@@ -3,7 +3,18 @@ import 'package:account_manager/business_logic/view_models/ledger.viewmodel.dart
 import 'package:flutter/foundation.dart';
 
 class TransactionViewModel extends ChangeNotifier {
-  List<Transaction> transactionList = [];
+  List<Transaction> transactionList = [
+    Transaction(
+      id: 1,
+      amount: 1000,
+      particular: 'Dawhkan',
+      date: DateTime.now(),
+      mode: 0,
+      transactionTypeId: 1,
+      baOrPektlak: 0,
+      cashOrBank: 2,
+      ),
+  ];
   int transactionTypeId;
   void addTransaction(Transaction transaction) {
     transactionList.add(transaction);
@@ -17,6 +28,7 @@ class TransactionViewModel extends ChangeNotifier {
   void newTransaction(Transaction transaction) {
     transactionList.add(transaction);
     if(transaction.mode==0){
+      // if(transaction.transactionType)
       // Leina
       // --find cash account and get its id
       // transaction type a zir in ledger involve tur zat a tam in a 
