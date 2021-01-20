61a54c36146169c1fddabbe0eeb1e731928ef329
sandarosm <sandaroslalmuanpuia@gmail.com>
2021-01-20 09:36:31 +0530

    Update dashBoard.screen.dart

--- lib/views/screens/dashBoard.screen.dart
+++ lib/views/screens/dashBoard.screen.dart
@@ -16,7 +16,7 @@ class _DashBoardState extends State<DashBoard> {
 int theriGroupVakue = 0;
 
 final Map<int, Widget> logoWidgets = const<int,Widget>{
-
+  
   0:Text("Ledgers"),
   1:Text("Transactions"),
   2:Text("Parties")
@@ -30,7 +30,7 @@ static Widget giveCenter(String yourText){
 }
 
 List<Widget> bodies = [
-  First(),
+ // First(),
   giveCenter("Home Page"),
   giveCenter("Search Page"),
   giveCenter("Chat Room")
@@ -44,9 +44,9 @@ List<Widget> bodies = [
         elevation: 2.0,
         backgroundColor: Colors.white,
         centerTitle: true,
-      //  title: Text("Title",style: TextStyle(color: Colors.black),),
+        title: Text("Current Month",style: TextStyle(color: Colors.black),),
         bottom: PreferredSize(
-          preferredSize: Size(double.infinity, 45.0),
+          preferredSize: Size(double.infinity, 75.0),
           child: Padding(
             padding: EdgeInsets.only(top: 5.0,bottom: 10.0),child: Row(
               children: <Widget>[
@@ -55,14 +55,96 @@ List<Widget> bodies = [
                 ),
 
                 Expanded(
-                  child: CupertinoSegmentedControl(
-                    groupValue:theriGroupVakue,
-                    onValueChanged: (changeFromGroupValue){
-                      setState(() {
-                        theriGroupVakue =changeFromGroupValue;
-                      });
-                    },
-                    children: logoWidgets,
+                  
+                  child: Column(
+                       crossAxisAlignment: CrossAxisAlignment.center,
+              
+                    children: [
+                   
+                      // Container(
+                      //     child: Center(
+                      //       child: Text(
+                      //         'CURRENT MONTH',
+                      //         style: TextStyle(
+                      //           fontWeight: FontWeight.w500,
+                      //           fontSize: 16,
+                      //         ),
+                      //       ),
+                      //     ),
+                      //   ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Material(
+                            borderRadius: BorderRadius.circular(10),
+                            elevation: 5,
+                          child: Container(
+                               //   padding: EdgeInsets.only(top: 4),
+                                  height: 40,
+                                  width: MediaQuery.of(context).size.width/2.5,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.white,
+                                    boxShadow: [BoxShadow(
+                                      color: Colors.cyan,
+                                      blurRadius: 0.5
+                                    )]
+                                  ),
+                              child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text('Pek Chhuah', textAlign: TextAlign.start, style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500,color: Colors.grey.shade500,),),
+                            SizedBox(height:3),
+                            Text('Rs 2000', textAlign: TextAlign.center, style: TextStyle(fontSize: 14, color: Colors.red[700]),),
+                                 
+                                ],
+                              ),
+                            ),
+                          ),
+                           
+                            Material(
+                          elevation: 5,
+                          borderRadius: BorderRadius.circular(10),
+                          child: Container(
+                            padding: EdgeInsets.only(top: 4),
+                            height: 40,
+                            width: MediaQuery.of(context).size.width/2.5,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              boxShadow: [BoxShadow(
+                                color: Colors.cyan,
+                                blurRadius: 0.5
+                              )]
+                            ),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text('Lakluh', textAlign: TextAlign.start, style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),),
+                                  SizedBox(height: 3),                          
+                                  Text('Rs 5000', textAlign: TextAlign.center, style: TextStyle(fontSize: 14, color: Colors.green[800]),)
+                                ],
+                              ),
+                          ),
+                )
+
+                        ],
+                      ),
+                     SizedBox(
+                  height: 15.0,
+                ),
+                       CupertinoSegmentedControl(
+                                  //  borderColor: Colors.red,
+                                    groupValue:theriGroupVakue,
+                                    onValueChanged: (changeFromGroupValue){
+                                      setState(() {
+                                        theriGroupVakue =changeFromGroupValue;
+                                      });
+                                    },
+                                    children: logoWidgets,
+                                  ),
+
+                    ],
                   ),
                 ),
 
@@ -81,111 +163,111 @@ List<Widget> bodies = [
 
 
 
-class First extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 50,),
-            Container(
-                child: Center(
-                  child: Text(
-                    'CURRENT MONTH',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 25,
-                    ),
-                  ),
-                ),
-              ),
-            SizedBox(height: 5,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Material(
-                  borderRadius: BorderRadius.circular(10),
-                  elevation: 5,
-                  child: Container(
-                    padding: EdgeInsets.only(top: 4),
-                    height: 80,
-                    width: MediaQuery.of(context).size.width/2.5,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                      boxShadow: [BoxShadow(
-                        color: Colors.cyan,
-                        blurRadius: 0.5
-                      )]
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Pek Chhuah', textAlign: TextAlign.start, style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500,color: Colors.grey.shade500,),),
-                        SizedBox(height:3),
-                        Text('Rs 2000', textAlign: TextAlign.center, style: TextStyle(fontSize: 15, color: Colors.red[700]),)
-                      ],
-                    ),
-                  ),
-                ),
-                Material(
-                  elevation: 5,
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    padding: EdgeInsets.only(top: 4),
-                    height: 80,
-                    width: MediaQuery.of(context).size.width/2.5,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                      boxShadow: [BoxShadow(
-                        color: Colors.cyan,
-                        blurRadius: 0.5
-                      )]
-                    ),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Lakluh', textAlign: TextAlign.start, style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500, color: Colors.grey.shade500,),),
-                          SizedBox(height: 3),                          
-                          Text('Rs 5000', textAlign: TextAlign.center, style: TextStyle(fontSize: 15, color: Colors.green[800]),)
-                        ],
-                      ),
-                  ),
-                )
-              ],
-            ),
-            SizedBox(height: 15,),
-            Categories(),
-            Divider(color: Colors.black,),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('CLD Enterprise', textAlign: TextAlign.left, style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),),
-                  Text('Rs 5000', textAlign: TextAlign.right, style: TextStyle(color:Colors.green, fontSize: 15, fontWeight: FontWeight.w300),),
-                ],
-              ),
-            ),
-            Divider(color: Colors.grey,),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Rics Technology', textAlign: TextAlign.left, style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),),
-                  Text('Rs 9000', textAlign: TextAlign.right, style: TextStyle(color:Colors.green, fontSize: 15, fontWeight: FontWeight.w300),),
-                ],
-              ),
-            )
-          ],
-        ),
-    );
-  }
-}
+// class First extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//         padding: const EdgeInsets.symmetric(horizontal: 20),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: <Widget>[
+//             SizedBox(height: 50,),
+//             Container(
+//                 child: Center(
+//                   child: Text(
+//                     'CURRENT MONTH',
+//                     style: TextStyle(
+//                       fontWeight: FontWeight.w500,
+//                       fontSize: 25,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             SizedBox(height: 5,),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//                 Material(
+//                   borderRadius: BorderRadius.circular(10),
+//                   elevation: 5,
+//                   child: Container(
+//                     padding: EdgeInsets.only(top: 4),
+//                     height: 80,
+//                     width: MediaQuery.of(context).size.width/2.5,
+//                     decoration: BoxDecoration(
+//                       borderRadius: BorderRadius.circular(10),
+//                       color: Colors.white,
+//                       boxShadow: [BoxShadow(
+//                         color: Colors.cyan,
+//                         blurRadius: 0.5
+//                       )]
+//                     ),
+//                     child: Column(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: [
+//                         Text('Pek Chhuah', textAlign: TextAlign.start, style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500,color: Colors.grey.shade500,),),
+//                         SizedBox(height:3),
+//                         Text('Rs 2000', textAlign: TextAlign.center, style: TextStyle(fontSize: 15, color: Colors.red[700]),)
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//                 Material(
+//                   elevation: 5,
+//                   borderRadius: BorderRadius.circular(10),
+//                   child: Container(
+//                     padding: EdgeInsets.only(top: 4),
+//                     height: 80,
+//                     width: MediaQuery.of(context).size.width/2.5,
+//                     decoration: BoxDecoration(
+//                       borderRadius: BorderRadius.circular(10),
+//                       color: Colors.white,
+//                       boxShadow: [BoxShadow(
+//                         color: Colors.cyan,
+//                         blurRadius: 0.5
+//                       )]
+//                     ),
+//                     child: Column(
+//                         mainAxisAlignment: MainAxisAlignment.center,
+//                         children: [
+//                           Text('Lakluh', textAlign: TextAlign.start, style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500, color: Colors.grey.shade500,),),
+//                           SizedBox(height: 3),                          
+//                           Text('Rs 5000', textAlign: TextAlign.center, style: TextStyle(fontSize: 15, color: Colors.green[800]),)
+//                         ],
+//                       ),
+//                   ),
+//                 )
+//               ],
+//             ),
+//             SizedBox(height: 15,),
+//             Categories(),
+//             Divider(color: Colors.black,),
+//             Container(
+//               padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Text('CLD Enterprise', textAlign: TextAlign.left, style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),),
+//                   Text('Rs 5000', textAlign: TextAlign.right, style: TextStyle(color:Colors.green, fontSize: 15, fontWeight: FontWeight.w300),),
+//                 ],
+//               ),
+//             ),
+//             Divider(color: Colors.grey,),
+//             Container(
+//               padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Text('Rics Technology', textAlign: TextAlign.left, style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),),
+//                   Text('Rs 9000', textAlign: TextAlign.right, style: TextStyle(color:Colors.green, fontSize: 15, fontWeight: FontWeight.w300),),
+//                 ],
+//               ),
+//             )
+//           ],
+//         ),
+//     );
+//   }
+// }
 
 class Categories extends StatefulWidget {
   @override
