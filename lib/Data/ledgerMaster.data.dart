db00cac9200df6c36ecac6fbc0fafc99e5dee1e5
tluanga <tluanga@live.com>
2021-01-22 12:19:24 +0530

    a

--- lib/Data/ledgerMaster.data.dart
+++ lib/Data/ledgerMaster.data.dart
@@ -1,19 +1,18 @@
 import 'package:account_manager/business_logic/models/ledgermaster.models.dart';
 
 List<LedgerMaster> ledgerMasterDataList = [
- LedgerMaster(id: 1, name: 'Cash Account', ledgerType: 1),
- LedgerMaster(id: 2, name: 'Machinery Account', ledgerType: 2),
- LedgerMaster(id: 3, name: 'Purchase Account', ledgerType: 3),
- LedgerMaster(id: 4, name: 'Purchase Account1', ledgerType: 4),
- LedgerMaster(id: 5, name: 'Purchase Account2', ledgerType: 5),
- LedgerMaster(id: 6, name: 'Purchase Account3', ledgerType: 6),
- LedgerMaster(id: 7, name: 'Purchase Account4', ledgerType: 7),
- LedgerMaster(id: 8, name: 'Purchase Account5', ledgerType: 8),
- LedgerMaster(id: 9, name: 'Purchase Account6', ledgerType: 9),
- LedgerMaster(id: 10, name: 'Purchase Account7', ledgerType: 10),
- LedgerMaster(id: 11, name: 'Purchase Account8', ledgerType: 11),
- LedgerMaster(id: 12, name: 'Purchase Account9', ledgerType: 12),
- LedgerMaster(id: 13, name: 'Purchase Account10', ledgerType: 13),
- LedgerMaster(id: 14, name: 'Purchase Account11', ledgerType: 14),
-
+  LedgerMaster(id: 1, name: 'Cash Account'),
+  LedgerMaster(id: 2, name: 'Machinery Account'),
+  LedgerMaster(id: 3, name: 'Purchase Account'),
+  LedgerMaster(id: 4, name: 'Purchase Account1'),
+  LedgerMaster(id: 5, name: 'Purchase Account2'),
+  LedgerMaster(id: 6, name: 'Purchase Account3'),
+  LedgerMaster(id: 7, name: 'Purchase Account4'),
+  LedgerMaster(id: 8, name: 'Purchase Account5'),
+  LedgerMaster(id: 9, name: 'Purchase Account6'),
+  LedgerMaster(id: 10, name: 'Purchase Account7'),
+  LedgerMaster(id: 11, name: 'Purchase Account8'),
+  LedgerMaster(id: 12, name: 'Purchase Account9'),
+  LedgerMaster(id: 13, name: 'Purchase Account10'),
+  LedgerMaster(id: 14, name: 'Purchase Account11'),
 ];
