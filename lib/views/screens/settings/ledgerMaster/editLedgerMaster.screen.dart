776ff0dda568cabf836771748864a38aabff56bd
tluanga <tluanga@live.com>
2021-01-27 00:10:51 +0530

    edit ledger master

--- lib/views/screens/settings/ledgerMaster/editLedgerMaster.screen.dart
+++ lib/views/screens/settings/ledgerMaster/editLedgerMaster.screen.dart
@@ -1,27 +1,122 @@
-import 'package:account_manager/business_logic/view_models/settings/ledgerMaster/editLedgerMaster.viewmodel.dart';
+import 'package:account_manager/business_logic/models/ledgermaster.models.dart';
+import 'package:account_manager/business_logic/view_models/settings/ledgerMaster/newLedgerMaster.viewmodel.dart';
+import 'package:account_manager/services/serviceLocator.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class EditLedgerMasterScreen extends StatefulWidget {
-  EditLedgerMasterScreen({Key key, int ledgerMasterId}) : super(key: key);
+  const EditLedgerMasterScreen({Key key}) : super(key: key);
 
   @override
   _EditLedgerMasterScreenState createState() => _EditLedgerMasterScreenState();
 }
 
 class _EditLedgerMasterScreenState extends State<EditLedgerMasterScreen> {
+  String _name;
+  String _description;
+  final _formKey = GlobalKey<FormState>();
+
+  void _onSubmit() {
+    if (_name != null && _description != null) {
+      NewLedgerMasterViewModel _ledgerMasterViewModel =
+          serviceLocator<NewLedgerMasterViewModel>();
+      _ledgerMasterViewModel.newLedgerMaster(
+        LedgerMaster(
+          name: _name,
+          description: _description,
+        ),
+      );
+
+      Navigator.pop(context);
+    }
+    if (_formKey.currentState.validate()) {
+      // If the form is valid, display a snackbar. In the real world,
+      // you'd often call a server or save the information in a database.
+
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Processing Data'),
+        ),
+      );
+    }
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
         title: Text('Edit Ledger Master'),
       ),
-      body: Consumer<EditLedgerMasterViewModel>(
-        builder: (contex, model, index) {
-          return Container(
-            child: Text(model.ledgerMasterForUpdate.name),
-          );
-        },
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: Container(
+            width: 350,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Name'),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter name';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      _name = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Description'),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter description';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      _description = value;
+                    },
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    _onSubmit();
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 400,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade300,
+                        border: Border.all(
+                          color: Colors.blue,
+                        ),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Submit',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
