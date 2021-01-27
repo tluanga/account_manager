f72b28be039069c85a9dbff92f4eaff83ba9e7bb
tluanga <tluanga@live.com>
2021-01-27 20:12:40 +0530

    dashboard transaction widget

--- lib/views/dashboard/transaction.widget.dart
+++ lib/views/dashboard/transaction.widget.dart
@@ -1,5 +1,6 @@
 import 'package:account_manager/business_logic/view_models/dashboard/dashboard.viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class TransactionDashboardWidget extends StatelessWidget {
@@ -13,6 +14,7 @@ class TransactionDashboardWidget extends StatelessWidget {
         print(
           model.lastTenTransactions.length.toString(),
         );
+
         return Container(
           child: Column(
             children: [
@@ -21,8 +23,55 @@ class TransactionDashboardWidget extends StatelessWidget {
                 child: ListView.builder(
                   itemCount: model.lastTenTransactions.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      child: Text('data'),
+                    var format = new DateFormat("yMMMMd");
+                    var dateString =
+                        format.format(model.lastTenTransactions[index].date);
+                    return Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 2,
+                      ),
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.blue.shade400,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(model
+                                      .lastTenTransactions[index].particular),
+                                  Text(
+                                    model.lastTenTransactions[index].amount
+                                        .toString(),
+                                  )
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(dateString),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.green.shade200,
+                                    ),
+                                    child: Text('Debit'),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
                     );
                   },
                 ),
