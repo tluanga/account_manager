da71794a6cb5b1d2a26ea03a40c9bddaffe5e1ee
tluanga <tluanga@live.com>
2021-02-04 09:10:25 +0530

    a

---
+++ lib/services/Reports/tradingAccount/tradingAccount/tradingAccount_impl.service.dart
@@ -0,0 +1,16 @@
+import 'package:account_manager/business_logic/models/ledgermaster.models.dart';
+
+abstract class LedgerMasterService {
+  //Future<List<LedgerMaster>> getLedgerMasterList(databaseProvider);
+  Future<List<LedgerMaster>> getList();
+  Future<LedgerMaster> getLedgerMaster(int id);
+  Future<int> insert(LedgerMaster ledgerMaster);
+  Future<int> update(LedgerMaster ledgerMaster);
+  Future<int> delete(int id);
+  Future<String> getLedgerMasterName(int _id);
+  Future<List<LedgerMaster>> getPartyList();
+  Future<List<LedgerMaster>> getAssetLedgerList();
+  Future<List<LedgerMaster>> getFilterdLedgerList(String _searchString);
+  Future<List<LedgerMaster>> getFilterdPartyLedgerList(String _searchString);
+  Future<List<LedgerMaster>> getTopTenLedgerList();
+}
